34782de2afb4c959bc0164f3a8d3e1b8bfced04c
frank <gwadafrank@gmail.com>
2017-11-21 19:37:50 +0300

    create test casese for auth and events

---
+++ test/test_auth_app.py
@@ -0,0 +1,17 @@
+import unittest
+
+class UserAuthTestcase(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def test_user_registration(self):
+        pass
+    
+    def test_user_login(self):
+        pass
+
+    def test_failed_login(self):
+        pass
+
+    def test_duplicate_usernames(self):
+        pass
---
+++ test/test_events_app.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestEventsItem(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    
+    def test_create_event(self):
+        pass
+    
+    def test_update_event(self):
+        pass
+
