c5a12cd485338370227163b3a1eb2dca2bef59ea
frank <gwadafrank@gmail.com>
2017-11-22 20:23:30 +0300

    update event_update Test case

--- test_events_app.py
+++ test_events_app.py
@@ -10,17 +10,25 @@ class TestEventsItem(unittest.TestCase):
         self.client = self.app.test_client
         self.new_event = {"text": "andela bootcamp"}
         
-    def test_retrieve_egit logvents(self):
+    def test_retrieve_events(self):
         resp = self.client().post('/api/events', data=self.new_event)
         self.assertEqual(resp.status_code, 201)
         resp = self.client().get('/api/events')
         self.assertIn('andela bootcamp', str(resp.data))
 
     def test_create_event(self):
-        pass
+        resp = self.client().post('/api/events', data=self.new_event)
+        self.assertEqual(resp.status_code, 201)
+        self.assertIn('andela bootcamp', str(resp.data))
     
     def test_update_event(self):
-        pass
+        resp = self.client().post('api/events', data=self.new_event)
+        self.assertEqual(resp.status_code, 201)
+        resp = self.client().put('/api/events/0', 
+                data={"text":"andela bootcamp cohort 24"})
+        self.assertEqual(resp.status_code, 200)
+        new_ = self.client().get('api/events/0')
+        self.assertIn('cohort', str(new_.data))
 
 
 if __name__ == '__main__':
