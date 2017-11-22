d0a3c35342b59dcee9bdbe69c93ebf72dca57d63
frank <gwadafrank@gmail.com>
2017-11-22 18:40:48 +0300

    add events to dictonary

--- app/views.py
+++ app/views.py
@@ -4,11 +4,7 @@ from flask_api import FlaskAPI, status, exceptions
 
 from app import app
 
-events = {
-    0: 'dev fest',
-    1: 'andela bootcamp',
-    2: 'for loop nairobi',
-}
+events = {}
 def api_view(key):
         return {
             'text': events[key]
@@ -19,5 +15,16 @@ def events_list():
     """
     List or create notes.
     """
+    if request.method == 'POST':
+        name = str(request.data.get('text',''))
+        # if the dictonary is empty assign id manualy
+        if not events.keys():
+            ids_ = 0
+        else:
+            ids_ = max(events.keys())+1
+        
+        events[ids_] = name
+        return api_view(ids_), status.HTTP_201_CREATED
+
     # request.method == 'GET'
-    return [api_view(idx) for idx in sorted(events.keys())]
+    return [api_view(ids_) for ids_ in sorted(events.keys())]
