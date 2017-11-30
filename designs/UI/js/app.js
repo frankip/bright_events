f626c33d715dce00a72feb044af3894859ed1f78
frank <gwadafrank@gmail.com>
2017-11-30 15:26:20 +0300

    retrieve events from api to html

--- designs/UI/js/app.js
+++ designs/UI/js/app.js
@@ -34,3 +34,35 @@ $(".tab a").on("click", function (e) {
     $(".tab-content > div").not(target).hide();
     $(target).fadeIn(600);
 });
+$(document).ready(function () {
+    loadfunctionAjax();
+});
+var loadfunctionAjax = function () {
+    $.ajax({
+        type: 'GET'
+        , url: 'https://eventsbright.herokuapp.com/api/events'
+        , contentType: "application/json; charset=utf-8"
+        , dataType: "json"
+        , success: function (data) {
+            console.log(data)
+            var trHTML = '';
+            $.each(data, function (i, item) {
+                console.log(data[i].event.name)
+                console.log(data[i].url)
+                trHTML += '<div class ="column" >'+
+                            '<div class ="callout" >'+
+                                '<img class = "thumbnail" src = "http://lorempixel.com/400/200/nightlife" alt="image-one">'+
+                                    '<h5>'+data[i].event.name+'</h5>' +
+                                    '<i class="fa fa-calendar fa-2x fa-fw" aria-hidden="true"></i>'+
+                                    '<p class="small">'+data[i].event.date+'</p>'+
+                                '<br>'+
+                                '<i class="fa fa-map-marker fa-2x fa-fw" aria-hidden="true"></i>'+
+                            '<p class = "small" >'+data[i].event.location+'</p>'+
+                        '<a href = "'+data[i].url+'" class = "button small expanded hollow" > View more details </a>' + 
+                    '</div >'+
+                '</div>'
+            });
+            $('#eventCard').append(trHTML);
+        }
+    });
+}
