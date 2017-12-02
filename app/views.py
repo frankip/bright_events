34effc5a359242053d43fa6bfe9caedf7b68e6f9
frank <gwadafrank@gmail.com>
2017-12-02 10:26:45 +0300

    update test

--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
 """
 this files contains the logic and the routes of the app
 """
-from flask import request, url_for, abort, g, jsonify, session
+from flask import request, url_for, abort, g, session
 from flask_api import status, exceptions
 
 from app import app
 from . models import Users, Events
+
 
 @app.route('/api/auth/register', methods=['POST'])
 def registration():
@@ -18,47 +19,50 @@ def registration():
     user = Users(email, password)
     if email in Users.user_db.keys():
         message = {"message": "User already exists. Please login."}
-        return message , status.HTTP_202_ACCEPTED
+        return message,status.HTTP_202_ACCEPTED
     user.save()
     return {'message': "user has been created"}, status.HTTP_201_CREATED
+
 
 @app.route('/api/auth/login', methods=['POST'])
 def login():
     """Handles the user login logic """
-    username = request.json.get('username')
-    password = request.json.get('password')
+    email = request.data.get('email')
+    password = request.data.get('password')
 
-
-    if username is None or password is None:
+    if email is None or password is None:
         message = {'message': 'inputs cannot be empty'}
-        return message , status.HTTP_400_BAD_REQUEST
+        return message, status.HTTP_400_BAD_REQUEST
 
     if not Users.user_db.keys():
         message = {"message": "You need to register first before you login"}
         return message, status.HTTP_400_BAD_REQUEST
 
-    user = Users(username, password)
-    if username in Users.user_db.keys():
-        if Users.user_db[username] == password:
+    user = Users(email, password)
+    if email in Users.user_db.keys():
+        if Users.user_db[email] == password:
             g.user = user
-            session['user'] = g.user.username
-            return {'message': 'you have succesfully been logged in'}, status.HTTP_200_OK
+            session['user'] = g.user.email
+            return {
+                'message': 'you have succesfully been logged in'}, status.HTTP_200_OK
         else:
             raise exceptions.AuthenticationFailed()
     raise exceptions.AuthenticationFailed()
+
 
 @app.route('/api/auth/logout', methods=['POST'])
 def logout():
     """Handles the user logout logic"""
     if 'user' not in session:
         message = {"message": "you have to login first"}
-        return message , status.HTTP_401_UNAUTHORIZED
+        return message, status.HTTP_401_UNAUTHORIZED
     session.pop('user')
     message = {"message": "you have been logged out"}
-    return message , status.HTTP_200_OK
+    return message, status.HTTP_200_OK
 
 # app.route('/api/auth/reset-password', methods=['POST'])
 # def reset_password():
+
 
 def api_view(key):
     """Handles how the data will be in the browsable api"""
@@ -68,6 +72,7 @@ def api_view(key):
         'event': Events.events_db[key],
     }
 
+
 @app.route("/api/events", methods=['GET', 'POST'])
 def events_list():
     """List or create events."""
@@ -75,7 +80,7 @@ def events_list():
         event = request.data.get('event')
         location = request.data.get('location')
         date = request.data.get('date')
-        
+
         if event is None or location is None or date is None:
             return {'message': 'inputs cannot be empty, please fill all inputs'}
 
@@ -86,6 +91,7 @@ def events_list():
 
     # request.method == 'GET'
     return [api_view(ids_) for ids_ in sorted(Events.events_db.keys())]
+
 
 @app.route("/api/events/<int:key>/", methods=['GET', 'PUT', 'DELETE'])
 def events_details(key):
@@ -130,5 +136,5 @@ def rsvp_event(key):
         if email is None:
             return{"message": "can not rsvp empty inputs"}
         rsvp_list.append(email)
-        return {'message':rsvp_list}, status.HTTP_201_CREATED
+        return {'message': rsvp_list}, status.HTTP_201_CREATED
     return rsvp_list
