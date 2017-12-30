7cb4f51ad1a6543286b39969c83d5c07aa78b68e
frank <gwadafrank@gmail.com>
2017-12-30 15:51:44 +0300

    [feature]Implement JWT token generation

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 """
 This files handles all the database logic and instances
 """
+import jwt
+from datetime import datetime, timedelta
 from passlib.apps import custom_app_context as pwd_context
 
 from app import db
@@ -56,6 +58,40 @@ class Users(db.Model):
         """
         return pwd_context.verify(self.password, password, )
 
+    def generate_token(self, user_id):
+        """Generating the access token"""
+        try:
+            #set up payload with an expiration time
+            payload = {
+                'exp': datetime.utcnow() + timedelta(minutes=5),
+                'iat': datetime.utcnow(),
+                'sub': user_id
+            }
+            # create the byte string token using the payload and the SECRET key
+            jwt_string = jwt.encode(
+                payload,
+                current_app.config.get('secret'),
+                algorithim='HS256'
+            )
+            return jwt_string
+
+        except Exception as e:
+            # return an error in string format if an exception occurs
+            return str(e)
+        
+    @staticmethod
+    def decode_token(token):
+        """Decodes the access token from the Authorization header."""
+        try:
+            # try to decode the token using our SECRET variable
+            payload = jwt.decode(token, current_app.config.get('SECRET'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            # The token is expired, return an error string
+            return "Expired token. Please login to get a new token"
+        except jwt.InvalidTokenError:
+            #The token is invalid, return an error string
+            return "Invalid token. Please register or login"
 
 class Events(db.Model):
     """
